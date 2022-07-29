a844650885fac76bcf18a6b9858a5576388ac31b
Egor Geberleyn <egeberleyn@gmail.com>
2022-07-29 23:25:53 +0300

    add shopcart repository, shopping cart design started

---
+++ Data/Interfaces/IShopCartRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ClothingStore.Data.Interfaces
+{
+    public interface IShopCartRepository
+    {
+        ShopCart GetAsync(IServiceProvider service);
+        void AddItemAsync(Product item);
+        void DeleteItemAsync(int id);
+    }
+}
---
+++ Data/Repository/ShopCartRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace ClothingStore.Data.Repository
+{
+    public class ShopCartRepository : IShopCartRepository
+    {        
+        private readonly ShopCart shopCart;
+       
+        public void AddItemAsync(Product item)
+        {
+            shopCart.ShopCartItems.Add(new ShopCartItem
+            {
+                Product = item,
+                ShopCartId = shopCart.Id
+            });
+        }
+
+        public void DeleteItemAsync(int id)
+        {
+            var item = shopCart.ShopCartItems.FirstOrDefault(p => p.Id == id);
+            shopCart.ShopCartItems.Remove(item);
+        }
+
+        public ShopCart GetAsync(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            var context = services.GetService<ShopDbContext>();
+            byte[] shopCartId = session.Get("CartId") ?? Guid.NewGuid().ToByteArray();
+
+            session.Set("CartId", shopCartId);
+            shopCart.Id = BitConverter.ToInt32(shopCartId, 0);
+            return shopCart;
+        }
+    }
+}
